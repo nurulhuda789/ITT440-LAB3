593fca86f1e888ae749cf7bf363bc3b5ac197d6f
murulhuda789 <hudasallehudin789@gmail.com>
2020-10-30 16:01:30 +0800

    3.1.c is commit

--- 3.1.c
+++ 3.1.c
@@ -8,11 +8,26 @@ int main(void)
 
   void sigint_handler(int sig);
   char s[200];
+  void sigtstp_handler(int sig);
+  void sigquit_handler(int sig); 
+
 
   if (signal(SIGINT, sigint_handler) == SIG_ERR){
      perror("signal");
      exit(1);
+    }
+
+  if (signal(SIGTSTP, sigtstp_handler) == SIG_ERR){
+     perror("signal");
+     exit(1);
+   }
+  
+  if (signal(SIGQUIT, sigquit_handler) == SIG_ERR){
+     perror("signal");
+     exit(1);
   }
+
+
 
   printf("Enter a string:\n");
 
@@ -26,6 +41,16 @@ int main(void)
 
 void sigint_handler(int sig)
 {
-  printf("Jangan Kacau saya !!!!! \n");
+  printf("This is a special signal handler for SIGINT   \n");
 }
+
+void sigtstp_handler(int sig)
+{
+  printf("This is a special signal handler for SIGTSTP   \n");
+}
+void sigquit_handler(int sig)
+{
+  printf("This is a special signal handler for SIGQUIT \n");
+}
+
 
